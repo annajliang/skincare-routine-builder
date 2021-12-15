1a5aeb2db428560383c8198067e769b33b3c1f88
annajliang <annaliangdev@gmail.com>
2021-12-15 00:50:39 -0500

    commit questions data array

---
+++ data/questions.ts
@@ -0,0 +1,64 @@
+const questions: { id: string, questionSpanOne: string, questionSpanTwo: string, options: string[] }[] = [
+    {
+        id: "1",
+        questionSpanOne: "How would you describe",
+        questionSpanTwo: "your skin type?",
+        options: [`I'll just say "oil slick" and leave it at that`, "Normal. Aren't I lucky?", "Combination -- dry here, oily there, just right in other spots", "Drier than the Sahara", "Always red, itchy & and irritated"]
+    },
+    {
+        id: "2",
+        questionSpanOne: "What is your",
+        questionSpanTwo: "top skin concern?",
+        options: ["Acne & blemishes", "Aging", "Dryness", "Blackheads & visible pores", "Acne scars & hyperpigmentation"]
+    },
+    {
+        id: "3",
+        questionSpanOne: "How much time do you prefer",
+        questionSpanTwo: "to spend on your routine?",
+        options: ["Time is precious so the shorter the better", "I don't mind dedicating a little extra time to it, but let's not go crazy here", "As long as possible, my skincare routine is my self-care ritual"]
+    },
+    {
+        id: "4",
+        questionSpanOne: "How much makeup",
+        questionSpanTwo: "do you wear on the daily?",
+        options: ["I don't wear any", "I go for a very minimal and natural makeup look", "A decent amount, but not full coverage", "I always go full out glam"]
+    },
+    {
+        id: "5",
+        questionSpanOne: "Do you enjoy when your",
+        questionSpanTwo: "products are scented?",
+        options: ["Yes", "No", "No preference"]
+    },
+    {
+        id: "6",
+        questionSpanOne: "What's missing from",
+        questionSpanTwo: "your routine?",
+        options: ["Retinol", "Sheet mask", "Hydrating serum", "Chemical exfoliant"]    
+    },
+    {
+        id: "7",
+        questionSpanOne: "How much money do you",
+        questionSpanTwo: "prefer to spend on products?",
+        options: ["As little as possible", "I'm ok with spending a little more than average", "I'm willing to spend any amount if the products work"]    
+    },
+     {
+        id: "8",
+        questionSpanOne: "How would you",
+        questionSpanTwo: "describe your skin tone?",
+        options: ["Fair", "Light", "Medium", "Olive", "Dark"]    
+    },
+    {
+        id: "9",
+        questionSpanOne: "Your average daily",
+        questionSpanTwo: "sun exposure is:",
+        options: ["Less than 20 minutes. I avoid the sun like the plague", "20-60 minutes. I'm all about moderation", "More than an hour. Call me a sun goddess"]    
+    },
+    {
+        id: "10",
+        questionSpanOne: "Cruelty-free & vegan",
+        questionSpanTwo: "skincare are important to you.",
+        options: ["Yes 100%", "No, I have no preference"]    
+    },
+]
+
+export default questions
