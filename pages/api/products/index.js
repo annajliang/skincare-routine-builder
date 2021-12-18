119d78fe47254ecd88d5124a0b534073ad182009
annajliang <annaliangdev@gmail.com>
2021-12-18 18:20:38 -0500

    setup get products route

---
+++ pages/api/products/index.js
@@ -0,0 +1,20 @@
+import dbConnect from "../../../utils/dbConnect";
+import Product from "../../../models/Product";
+
+dbConnect();
+
+const getProducts = async (req, res) => {
+    const { method } = req;
+    console.log(method)
+
+    if (method === 'GET') {
+        try {
+            const products = await Product.find();
+            res.status(200).json({ success: true, data: products })
+        } catch (err) {
+            res.status(400).json({ success: false });
+        }
+    }
+ }
+
+ export default getProducts;
