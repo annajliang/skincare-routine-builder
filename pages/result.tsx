aaa8bc46861d83e32e5365674d274b0d329a8531
annajliang <annaliangdev@gmail.com>
2021-12-20 04:00:21 -0500

    create result page

---
+++ pages/result.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import Calculating from "../client/components/Calculating";
+
+const Result: NextPage = () => {
+  return <Calculating />;
+};
+
+export default Result;
