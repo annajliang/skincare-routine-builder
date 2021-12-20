90cdbda4bc8fcb7dda09ff819fa0b58d4dedd02b
annajliang <annaliangdev@gmail.com>
2021-12-20 04:03:13 -0500

    create IProducts interface and get products through fetch request

--- client/components/Question.tsx
+++ client/components/Question.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import questions from "../data/questions";
 import { useRouter } from "next/router";
 import { Animated } from "react-animated-css";
@@ -28,10 +29,61 @@ export interface IUserChoices {
   answer: string | undefined
 }
 
+interface IProducts {
+  name: string;
+  description: string;
+  img_url: string;
+  ingredients: string;
+  texture: string;
+  price_range: string;
+  skin_type: string[];
+  has_fragrance: boolean;
+  has_alcohol?: boolean;
+  is_waterproof?: boolean; 
+  is_tinted?: boolean;
+  removes_makeup?: boolean; 
+  spf?: number; 
+  sunscreen_type?: string[]; 
+  category: string
+}
+
+
 const Question: React.FC = () => {
   const [userChoices, setUserChoices] = useState<Array<IUserChoices>>([]);
+  const [products, setProducts] = useState<Array<IProducts>>([]);
   const route = useRouter();
 
+  console.log("userChoices", userChoices);
+
+  useEffect(() => {
+    console.log("rendered");
+
+    const getProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/products");
+        console.log("response", response);
+
+        if (response.ok) {
+          const data = await response.json();
+          // console.log("data", data.data);
+          setProducts([...data.data]);
+          
+        } else {
+          throw new Error(response.statusText);
+        }
+
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    getProducts()
+    
+  }, []);
+
+    if (products.length !== 0) {
+      console.log('products',products);
+    }
   return (
     <StyledQuestion>
       {questions.map((question, i) => {
