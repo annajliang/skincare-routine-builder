834789c6e28867084ab4041f805436b0daed583d
annajliang <annaliangdev@gmail.com>
2021-12-22 14:32:46 -0500

    move userChoices state to parent _app cmponent, import Normalize and Global styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,49 @@
-import "../client/styles/normalize.css";
-import "../client/styles/globals.css";
+import React, { useState } from "react";
+import Global from "../client/styles/Global";
+import Normalize from "../client/styles/Normalize";
 import Layout from "../client/components/Layout";
 import type { AppProps } from "next/app";
 
+export interface IUserChoices {
+  id: string;
+  question: string;
+  answer: string | undefined;
+}
+
+interface IUserChoicesContext {
+  userChoices: IUserChoices;
+  setUserChoices: (arr: IUserChoices[]) => void;
+}
+
+
+export const UserChoicesContext = React.createContext<IUserChoicesContext>({
+  userChoices: {
+    id: "",
+    question: "",
+    answer: "",
+  },
+  setUserChoices: function (arr: IUserChoices[]) {},
+});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [userChoices, setUserChoices] = useState<Array<IUserChoices>>([]);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Normalize />
+      <Global />
+      <Layout>
+        <UserChoicesContext.Provider
+          value={{
+            userChoices,
+            setUserChoices,
+          }}
+        >
+          <Component {...pageProps} />
+        </UserChoicesContext.Provider>
+      </Layout>
+    </>
   );
 }
 
