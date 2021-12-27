7a35c2d75dcfb374e5861f5de671282fa77b0a7d
annajliang <annaliangdev@gmail.com>
2021-12-26 19:29:40 -0500

    show user their morning routine based on selected choices

--- client/components/ProductCard.tsx
+++ client/components/ProductCard.tsx
@@ -1,7 +1,11 @@
-import styled from "styled-components"; 
-import { RecommendedContext } from "../../pages/_app";
+import styled from "styled-components";
+import {
+  IProduct,
+  RecommendedContext,
+  UserChoicesContext,
+} from "../../pages/_app";
 import Link from "next/link";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 
 const StyledGridItem = styled.div`
   background: #ffffff;
@@ -9,7 +13,7 @@ const StyledGridItem = styled.div`
   color: #6f4938;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   /* justify-content: center; */
   padding: 1.5rem;
   text-align: center;
@@ -26,6 +30,7 @@ const StyledGridItem = styled.div`
     margin-top: 1rem;
     font-weight: bold;
     font-size: 1.6rem;
+    text-overflow: ellipsis;
   }
 `;
 
@@ -33,6 +38,11 @@ const StyledInnerGridItem = styled.div`
   background: #f2f0ed;
   width: 100%;
   height: 60%;
+  padding: 1rem 0;
+
+  img {
+    height: 100%;
+  }
 `;
 
 const StyledBottomGridBar = styled.a`
@@ -49,18 +59,76 @@ const StyledBottomGridBar = styled.a`
 `;
 
 const ProductCard = () => {
-    const { recommendedProducts } = useContext(RecommendedContext);
+  const { recommendedProducts } = useContext(RecommendedContext);
+  const { userChoices } = useContext(UserChoicesContext);
+  const [morningRoutine, setMorningRoutine] = useState<Array<IProduct>>([]);
+  const [nightRoutine, setNightRoutine] = useState<Array<IProduct>>([]);
+
+  useEffect(() => {
     console.log("product card", recommendedProducts);
+    console.log("userChoices", userChoices);
 
-    return (
-      <StyledGridItem>
-        <StyledInnerGridItem>test</StyledInnerGridItem>
-        <p>CeraVe Foaming Facial Cleanser</p>
-        <Link href="http://google.com" passHref>
-          <StyledBottomGridBar target="_blank">BUY NOW</StyledBottomGridBar>
-        </Link>
-      </StyledGridItem>
-    );
-}
+    const morningRoutineCopy: IProduct[] = [];
 
-export default ProductCard
+    const findProduct = (productType: string, arr: IProduct[]) => {
+      const filteredProducts = recommendedProducts.filter(
+        (recommendedProduct: {
+          [key: string]: string | string[] | boolean | number;
+        }) => recommendedProduct.category === productType
+      );
+
+      console.log(`FILTERED PRODUCTS - ${productType}`, filteredProducts);
+
+      // if there are more than 1 found product, pick a random one
+      if (filteredProducts.length > 1) {
+        const index = Math.floor(Math.random() * filteredProducts.length);
+        console.log("PRODUCT - IF", filteredProducts[index]);
+        arr.push(filteredProducts[index]);
+        console.log("morningRoutineCopy - IF", arr);
+        console.log("---------");
+        // add the single product to state
+      } else {
+        console.log("PRODUCT - ELSE", filteredProducts);
+        arr.push(...filteredProducts);
+        console.log("morningRoutineCopy - IF", arr);
+      }
+    };
+
+    if ( morningRoutine.length === 0) {
+
+        findProduct("moisturizer", morningRoutineCopy);
+        findProduct("cleanser", morningRoutineCopy);
+        findProduct("sunscreen", morningRoutineCopy);
+        console.log("morningRoutine", morningRoutine);
+        
+            setMorningRoutine(morningRoutineCopy);
+    }
+  }, [morningRoutine, recommendedProducts, userChoices]);
+
+  return (
+    <>
+      {morningRoutine.length !== 0 ? (
+        morningRoutine.map((recommendedProduct: IProduct, i: number) => {
+          return (
+            <StyledGridItem key={i}>
+              <StyledInnerGridItem>
+                {/* test */}
+                <img src={recommendedProduct.img_url} />
+              </StyledInnerGridItem>
+              <p>{recommendedProduct.name}</p>
+              <Link href="http://google.com" passHref>
+                <StyledBottomGridBar target="_blank">
+                  BUY NOW
+                </StyledBottomGridBar>
+              </Link>
+            </StyledGridItem>
+          );
+        })
+      ) : (
+        <div>NOT WORKING</div>
+      )}
+    </>
+  );
+};
+
+export default ProductCard;
