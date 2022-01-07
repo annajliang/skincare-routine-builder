0acfa991a45e7890eac48e4ad6913e1441654786
annajliang <annaliangdev@gmail.com>
2022-01-07 12:37:12 -0500

    fix typo

--- server/utils/dbConnect.js
+++ server/utils/dbConnect.js
@@ -7,7 +7,7 @@ const dbConnect = async () => {
     return;
   }
 
-  const db = await mongoose.connect('tes', {
+  const db = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
