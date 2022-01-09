511a3595dbe5b0f4902089b8c233cd239bdf1629
annajliang <annaliangdev@gmail.com>
2022-01-09 01:36:52 -0500

    undo stackoverflow fix

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,24 +1,19 @@
 import dbConnect from "../../../server/utils/dbConnect";
-import Product from "../../../models/Product";
-import mongoose from "mongoose";
+import Product from "../../../models/Product";;
 
 dbConnect();
 
 const getProducts = async (req, res) => {
     const { method } = req;
-    const session = await mongoose.startSession();
 
     if (method === 'GET') {
         try {
-          // get request fix - https://stackoverflow.com/questions/68607254/mongodb-node-js-driver-4-0-0-cursor-session-id-issues-in-production-on-vercel
-          const products = await Product.find().session(session);
+          const products = await Product.find();
           res.status(200).json({ success: true, data: products });
         } catch (err) {
           console.error(err);
           res.status(400).json({ success: false });
-        } finally {
-          await session.endSession();
-        }
+        } 
     }
  }
 
