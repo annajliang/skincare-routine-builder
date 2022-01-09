91e24e2d3a53cb730be4b821ff0a33a190d72342
annajliang <annaliangdev@gmail.com>
2022-01-09 01:01:20 -0500

    attempt to fix get request failure

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,15 +1,17 @@
 import dbConnect from "../../../server/utils/dbConnect";
 import Product from "../../../models/Product";
+import mongoose from "mongoose";
 
 dbConnect();
 
 const getProducts = async (req, res) => {
     const { method } = req;
+    const session = await mongoose.startSession();
 
     if (method === 'GET') {
         try {
           // get request fix - https://stackoverflow.com/questions/68607254/mongodb-node-js-driver-4-0-0-cursor-session-id-issues-in-production-on-vercel
-          const products = await Product.find().limit(200);
+          const products = await Product.find().session(session);
           res.status(200).json({ success: true, data: products });
         } catch (err) {
             console.error(err)
