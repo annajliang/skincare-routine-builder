3788b7d50dac52397cff123732afa92cfca29236
annajliang <annaliangdev@gmail.com>
2022-01-09 01:51:09 -0500

    rewrite dbConnect file

--- server/utils/dbConnect.js
+++ server/utils/dbConnect.js
@@ -1,19 +1,40 @@
 import mongoose from "mongoose";
 
-const connection = {};
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const dbConnect = async () => {
-  if (connection.isConnected) {
-    return;
+if (!MONGODB_URI) {
+  throw new Error(
+    "Please define the MONGODB_URI environment variable inside .env.local"
+  );
+}
+
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) {
+    return cached.conn;
   }
 
-  const db = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
 
-  connection.isConnected = db.connections[0].readyState;
-  console.log(connection.isConnected);
-};
+    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
 
 export default dbConnect;
