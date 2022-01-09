d089b7077cffd622e2b2719a9af1215b1e9e94fa
annajliang <annaliangdev@gmail.com>
2022-01-09 02:34:52 -0500

    remove mongoose, use json file to store api data instead

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -1,23 +1,14 @@
-import dbConnect from "../../../server/utils/dbConnect";
-import Product from "../../../models/Product";
-import mongoose from "mongoose";
-
-dbConnect();
+import data from "../../../products"
 
 const getProducts = async (req, res) => {
     const { method } = req;
-    const session = await mongoose.startSession();
 
     if (method === 'GET') {
         try {
-          // get request fix - https://stackoverflow.com/questions/68607254/mongodb-node-js-driver-4-0-0-cursor-session-id-issues-in-production-on-vercel
-          const products = await Product.find().session(session);
-          res.status(200).json({ success: true, data: products });
+          res.status(200).json( data );
         } catch (err) {
           console.error(err);
-          res.status(400).json({ success: false });
-        } finally {
-          await session.endSession();
+          res.status(400).json({ success: false, data: 'Failed to retrieve data.' });
         }
     }
  }
