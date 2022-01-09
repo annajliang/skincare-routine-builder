3091e83406ff07ab78bd307856588e25194dc168
annajliang <annaliangdev@gmail.com>
2022-01-08 20:39:17 -0500

    increase document limit to 200

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -8,8 +8,9 @@ const getProducts = async (req, res) => {
 
     if (method === 'GET') {
         try {
-            const products = await Product.find().limit(100);
-            res.status(200).json({ success: true, data: products })
+          // get request fix - https://stackoverflow.com/questions/68607254/mongodb-node-js-driver-4-0-0-cursor-session-id-issues-in-production-on-vercel
+          const products = await Product.find().limit(200);
+          res.status(200).json({ success: true, data: products });
         } catch (err) {
             console.error(err)
             res.status(400).json({ success: false });
